8f0d2c054c3a08086b1ff26c9e4f7eb044619dfb
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2024-05-18 11:39:36 +0530

    started cfg

---
+++ control-flow-graph/node_processor/__init__.py
@@ -0,0 +1,6 @@
+'''
+The Node Processor Sub-module
+
+Provides logic for node types and their class methods.
+Also contains the logic to traverse the AST to generate the CFG.
+'''
--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import json
 from compiler import SolCompiler
 
 source = '''
-pragma solidity >0.4.99 <0.6.0;
- 
-  library OldLibrary {
-    function someFunction(uint8 a) public returns(bool);
-  }
- 
-  contract NewContract {
-    function f(uint8 a) public returns (bool) {
-        return OldLibrary.someFunction(a);
+pragma solidity ^0.4.0;
+
+contract c {
+    int a = 10;
+    int b = 12;
+
+    function run() {
+        if (a == 10) {
+        b += 1;
+        } else {
+            b -= 1;
+        }
     }
-  }
+}
+
 '''
 
 compiler = SolCompiler(source)
 output = compiler.compile()
 contracts = output.get_contracts_list()
 print(contracts)
-ast = output.get_ast(contracts[1])
+ast = output.get_ast(contracts[0])
+
+print(ast.keys())
+
 with open('./gen/ast.json', 'w', encoding='utf8') as f:
     json.dump(ast, f, indent=4)
